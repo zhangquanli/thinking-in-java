a5cdebf8c901bc4658de38d3cc94e1f1ca5f089a
zhangquanli <502624709@qq.com>
2019-12-23 14:28:46 +0800

    项目结构调整

--- src/main/java/typeinfo/WildcardClassReferences.java
+++
@@ -1,9 +0,0 @@
-//: Java编程思想第四版第320-321页
-package typeinfo;
-
-public class WildcardClassReferences {
-    public static void main(String[] args) {
-        Class<?> intClass = int.class;
-        intClass = double.class;
-    }
-}
--- src/main/java/typeinfo/pets/Person.java
+++
@@ -1,8 +0,0 @@
-//: Java编程思想第四版第323页
-package typeinfo.pets;
-
-public class Person extends Individual {
-    public Person(String name) {
-        super(name);
-    }
-}

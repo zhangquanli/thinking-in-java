51d0f1c6af000685ca2e3a74e6a2c26d32f77999
zhangquanli <502624709@qq.com>
2019-12-26 13:56:05 +0800

    第19章 枚举类型

---
+++ thinkinginjava-src/src/main/java/enumerated/OverrideConstantSpecific.java
@@ -0,0 +1,29 @@
+//: enumerated/OverrideConstantSpecific.java
+package enumerated;
+
+import static net.mindview.util.Print.print;
+import static net.mindview.util.Print.printnb;
+
+public enum OverrideConstantSpecific {
+    NUT, BOLT,
+    WASHER {
+        void f() {
+            print("Overridden method");
+        }
+    };
+
+    void f() {
+        print("default behavior");
+    }
+
+    public static void main(String[] args) {
+        for (OverrideConstantSpecific ocs : values()) {
+            printnb(ocs + ": ");
+            ocs.f();
+        }
+    }
+} /* Output:
+NUT: default behavior
+BOLT: default behavior
+WASHER: Overridden method
+*///:~

09dbc07e642f7841a423039967377784ee46aafd
zhangquanli <502624709@qq.com>
2020-12-30 13:22:18 +0800

    第5章 初始化与清理

---
+++ thinkinginjava-src/src/main/java/initialization/ArrayNew.java
@@ -0,0 +1,22 @@
+//: initialization/ArrayNew.java
+// Creating arrays with new.
+package initialization;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import static net.mindview.util.Print.print;
+
+public class ArrayNew {
+    public static void main(String[] args) {
+        int[] a;
+        Random rand = new Random(47);
+        a = new int[rand.nextInt(20)];
+        print("length of a = " + a.length);
+        print(Arrays.toString(a));
+    }
+}
+/* Output:
+length of a = 18
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+*///:~

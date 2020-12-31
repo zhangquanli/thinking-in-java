a00782c37cd411de0391306b8311b2f33d7da679
zhangquanli <502624709@qq.com>
2020-12-31 09:46:55 +0800

    第6章 访问权限控制

---
+++ thinkinginjava-src/src/main/java/access/Dinner.java
@@ -0,0 +1,15 @@
+//: access/Dinner.java
+// Uses the library.
+package access;
+
+import access.dessert.Cookie;
+
+public class Dinner {
+    public static void main(String[] args) {
+        Cookie x = new Cookie();
+        //! x.bite(); // Can't access
+    }
+}
+/* Output:
+Cookie constructor
+*///:~
---
+++ thinkinginjava-src/src/main/java/access/Pie.java
@@ -0,0 +1,10 @@
+//: access/Pie.java
+// The other class.
+package access;
+
+class Pie {
+    void f() {
+        System.out.println("Pie.f()");
+    }
+}
+///:~

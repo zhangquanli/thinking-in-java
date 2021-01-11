a4d129587667aa85038e2ce7d49157d1f31377ed
zhangquanli <502624709@qq.com>
2021-01-11 08:57:45 +0800

    第7章 复用类

--- thinkinginjava-src/src/main/java/reusing/Beetle.java
+++ thinkinginjava-src/src/main/java/reusing/Beetle.java
@@ -1,35 +1,44 @@
-package reusing;//: reusing/Beetle.java
+//: reusing/Beetle.java
 // The full process of initialization.
-import static net.mindview.util.Print.*;
+package reusing;
+
+import static net.mindview.util.Print.print;
 
 class Insect {
-  private int i = 9;
-  protected int j;
-  Insect() {
-    print("i = " + i + ", j = " + j);
-    j = 39;
-  }
-  private static int x1 =
-    printInit("static Insect.x1 initialized");
-  static int printInit(String s) {
-    print(s);
-    return 47;
-  }
+    private int i = 9;
+    protected int j;
+
+    Insect() {
+        print("i = " + i + ", j = " + j);
+        j = 39;
+    }
+
+    private static int x1 =
+            printInit("static Insect.x1 initialized");
+
+    static int printInit(String s) {
+        print(s);
+        return 47;
+    }
 }
 
 public class Beetle extends Insect {
-  private int k = printInit("Beetle.k initialized");
-  public Beetle() {
-    print("k = " + k);
-    print("j = " + j);
-  }
-  private static int x2 =
-    printInit("static Beetle.x2 initialized");
-  public static void main(String[] args) {
-    print("Beetle constructor");
-    Beetle b = new Beetle();
-  }
-} /* Output:
+    private int k = printInit("Beetle.k initialized");
+
+    public Beetle() {
+        print("k = " + k);
+        print("j = " + j);
+    }
+
+    private static int x2 =
+            printInit("static Beetle.x2 initialized");
+
+    public static void main(String[] args) {
+        print("Beetle constructor");
+        Beetle b = new Beetle();
+    }
+}
+/* Output:
 static Insect.x1 initialized
 static Beetle.x2 initialized
 Beetle constructor

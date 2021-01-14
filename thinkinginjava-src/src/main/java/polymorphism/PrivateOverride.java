e21df3e0310c18e20f4694d463fe92ba519f09f4
zhangquanli <502624709@qq.com>
2021-01-14 12:51:36 +0800

    第8章 多态

--- thinkinginjava-src/src/main/java/polymorphism/PrivateOverride.java
+++ thinkinginjava-src/src/main/java/polymorphism/PrivateOverride.java
@@ -1,18 +1,25 @@
 //: polymorphism/PrivateOverride.java
 // Trying to override a private method.
 package polymorphism;
-import static net.mindview.util.Print.*;
+
+import static net.mindview.util.Print.print;
 
 public class PrivateOverride {
-  private void f() { print("private f()"); }
-  public static void main(String[] args) {
-    PrivateOverride po = new Derived();
-    po.f();
-  }
+    private void f() {
+        print("private f()");
+    }
+
+    public static void main(String[] args) {
+        PrivateOverride po = new Derived();
+        po.f();
+    }
 }
 
 class Derived extends PrivateOverride {
-  public void f() { print("public f()"); }
-} /* Output:
+    public void f() {
+        print("public f()");
+    }
+}
+/* Output:
 private f()
 *///:~
